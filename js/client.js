8955bd7f47e0176501888863c75ad90c15b7c291
Stevan Sehn <stevanlinse@gmail.com>
2020-08-18 08:37:23 -0300

    1st attempt

--- js/client.js
+++ js/client.js
@@ -1,174 +1,385 @@
-'use strict';
+"use strict";
 
-const startButton = document.getElementById('startButton');
-const callButton = document.getElementById('callButton');
-const hangupButton = document.getElementById('hangupButton');
-callButton.disabled = true;
-hangupButton.disabled = true;
-startButton.onclick = start;
-callButton.onclick = call;
-hangupButton.onclick = hangup;
-
-const video1 = document.querySelector('video#video1');
-const video2 = document.querySelector('video#video2');
-const video3 = document.querySelector('video#video3');
-
-let pc1Local;
-let pc1Remote;
-let pc2Local;
-let pc2Remote;
-const offerOptions = {
-    offerToReceiveAudio: 1,
-    offerToReceiveVideo: 1
+// Clean-up function:
+// collect garbage before unloading browser's window
+window.onbeforeunload = function (e) {
+  hangup();
 };
 
-function gotStream(stream) {
-    console.log('Received local stream');
-    video1.srcObject = stream;
-    window.localStream = stream;
-    callButton.disabled = false;
+// Data channel information
+var sendChannel, receiveChannel;
+// var sendButton = document.getElementById("sendButton");
+// var sendTextarea = document.getElementById("dataChannelSend");
+// var receiveTextarea = document.getElementById("dataChannelReceive");
+
+// HTML5 <video> elements
+// const video1 = document.querySelector('video#video1');
+// const video2 = document.querySelector('video#video2');
+// const video3 = document.querySelector('video#video3');
+
+var localVideo = document.querySelector("#video1");
+var remoteVideo = document.querySelector("#video2");
+var remoteVideo2 = document.querySelector("#video3");
+
+// Handler associated with 'Send' button
+// sendButton.onclick = sendData;
+
+// Flags...
+var isChannelReady;
+var isInitiator;
+var isStarted;
+var is2Client;
+
+// WebRTC data structures
+// Streams
+var localStream;
+var remoteStream;
+var remoteStream2;
+// Peer Connection
+var pc;
+var pc2;
+
+/////////////////////////////////////////////
+
+// Let's get started: prompt user for input (room name)
+var room = prompt("Enter room name:");
+
+// Connect to signalling server
+var socket = io.connect();
+
+// Send 'Create or join' message to singnalling server
+if (room !== "") {
+  console.log("Create or join room", room);
+  socket.emit("create or join", room);
 }
 
-function start() {
-    console.log('Requesting local stream');
-    startButton.disabled = true;
-    navigator.mediaDevices
-        .getUserMedia({
-            audio: true,
-            video: true
-        })
-        .then(gotStream)
-        .catch(e => console.log('getUserMedia() error: ', e));
+// Set getUserMedia constraints
+var constraints = { video: true };
+
+// Call getUserMedia()
+// navigator.getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(handleUserMedia)
+  .catch(handleUserMediaError);
+console.log("Getting user media with constraints", constraints);
+
+// From this point on, execution proceeds based on asynchronous events...
+
+/////////////////////////////////////////////
+
+// getUserMedia() handlers...
+/////////////////////////////////////////////
+function handleUserMedia(stream) {
+  // video1.srcObject = stream;
+  // window.localStream = stream;
+  localStream = stream;
+  // attachMediaStream(localVideo, stream);
+  localVideo.srcObject = stream;
+  console.log("Adding local stream.");
+  sendMessage("got user media");
+  if (isInitiator) {
+    checkAndStart();
+  }
 }
 
-function call() {
-    callButton.disabled = true;
-    hangupButton.disabled = false;
-    console.log('Starting calls');
-    const audioTracks = window.localStream.getAudioTracks();
-    const videoTracks = window.localStream.getVideoTracks();
-    if (audioTracks.length > 0) {
-        console.log(`Using audio device: ${audioTracks[0].label}`);
+function handleUserMediaError(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+/////////////////////////////////////////////
+
+// Server-mediated message exchanging...
+/////////////////////////////////////////////
+
+// 1. Server-->Client...
+/////////////////////////////////////////////
+
+// Handle 'created' message coming back from server:
+// this peer is the initiator
+socket.on("created", function (room) {
+  console.log("Created room " + room);
+  isInitiator = true;
+});
+
+// Handle 'full' message coming back from server:
+// this peer arrived too late :-(
+socket.on("full", function (room) {
+  console.log("Room " + room + " is full");
+});
+
+// Handle 'join' message coming back from server:
+// another peer is joining the channel
+socket.on("join", function (room) {
+  console.log("Another peer made a request to join room " + room);
+  console.log("This peer is the initiator of room " + room + "!");
+  isChannelReady = true;
+});
+
+// Handle 'joined' message coming back from server:
+// this is the second peer joining the channel
+socket.on("joined", function (room) {
+  console.log("This peer has joined room " + room);
+  isChannelReady = true;
+});
+
+// Handle third peer/2nd Client
+socket.on("joined2", function (room) {
+  console.log("This peer has joined room " + room);
+  is2Client = true;
+});
+
+// Server-sent log message...
+socket.on("log", function (array) {
+  console.log.apply(console, array);
+});
+
+// Receive message from the other peer via the signalling server
+socket.on("message", function (message) {
+  console.log("Received message:", message);
+  if (message === "got user media") {
+    checkAndStart();
+  } else if (message.type === "offer") {
+    if (!isInitiator && !isStarted) {
+      checkAndStart();
     }
-    if (videoTracks.length > 0) {
-        console.log(`Using video device: ${videoTracks[0].label}`);
+    pc.setRemoteDescription(new RTCSessionDescription(message));
+    doAnswer();
+  } else if (message.type === "answer" && isStarted) {
+    pc.setRemoteDescription(new RTCSessionDescription(message));
+  } else if (message.type === "candidate" && isStarted) {
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: message.label,
+      candidate: message.candidate,
+    });
+    pc.addIceCandidate(candidate);
+  } else if (message === "bye" && isStarted) {
+    handleRemoteHangup();
+  }
+});
+////////////////////////////////////////////////
+
+// 2. Client-->Server
+////////////////////////////////////////////////
+// Send message to the other peer via the signalling server
+function sendMessage(message) {
+  console.log("Sending message: ", message);
+  socket.emit("message", message);
+}
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+// Channel negotiation trigger function
+function checkAndStart() {
+  if (!isStarted && typeof localStream != "undefined" && isChannelReady) {
+    createPeerConnection();
+    pc.addStream(localStream);
+    isStarted = true;
+    if (isInitiator) {
+      doCall();
     }
-    // Create an RTCPeerConnection via the polyfill.
-    const servers = null;
-    pc1Local = new RTCPeerConnection(servers);
-    pc1Remote = new RTCPeerConnection(servers);
-    pc1Remote.ontrack = gotRemoteStream1;
-    pc1Local.onicecandidate = iceCallback1Local;
-    pc1Remote.onicecandidate = iceCallback1Remote;
-    console.log('pc1: created local and remote peer connection objects');
-
-    pc2Local = new RTCPeerConnection(servers);
-    pc2Remote = new RTCPeerConnection(servers);
-    pc2Remote.ontrack = gotRemoteStream2;
-    pc2Local.onicecandidate = iceCallback2Local;
-    pc2Remote.onicecandidate = iceCallback2Remote;
-    console.log('pc2: created local and remote peer connection objects');
-
-    window.localStream.getTracks().forEach(track => pc1Local.addTrack(track, window.localStream));
-    console.log('Adding local stream to pc1Local');
-    pc1Local
-        .createOffer(offerOptions)
-        .then(gotDescription1Local, onCreateSessionDescriptionError);
-
-    window.localStream.getTracks().forEach(track => pc2Local.addTrack(track, window.localStream));
-    console.log('Adding local stream to pc2Local');
-    pc2Local.createOffer(offerOptions)
-        .then(gotDescription2Local, onCreateSessionDescriptionError);
+  }
 }
 
-function onCreateSessionDescriptionError(error) {
-    console.log(`Failed to create session description: ${error.toString()}`);
+/////////////////////////////////////////////////////////
+// Peer Connection management...
+function createPeerConnection() {
+  try {
+    pc = new RTCPeerConnection();
+    pc.onicecandidate = handleIceCandidate;
+  } catch (e) {
+    console.log("Failed to create PeerConnection, exception: " + e.message);
+    alert("Cannot create RTCPeerConnection object.");
+    return;
+  }
+  pc.onaddstream = handleRemoteStreamAdded;
+  pc.onremovestream = handleRemoteStreamRemoved;
+
+  if (is2Client) {
+    try {
+      pc2 = new RTCPeerConnection();
+      pc2.onicecandidate = handleIceCandidate;
+    } catch (e) {
+      console.log("Failed to create PeerConnection, exception: " + e.message);
+      alert("Cannot create RTCPeerConnection object.");
+      return;
+    }
+    pc2.onaddstream = handleRemoteStreamAdded2;
+    pc2.onremovestream = handleRemoteStreamRemoved2;
+  }
+
+  // if (isInitiator) {
+  //   try {
+  //     // Create a reliable data channel
+  //     sendChannel = pc.createDataChannel("sendDataChannel");
+  //     trace("Created send data channel");
+  //   } catch (e) {
+  //     alert("Failed to create data channel. ");
+  //     trace("createDataChannel() failed with exception: " + e.message);
+  //   }
+  //   sendChannel.onopen = handleSendChannelStateChange;
+  //   sendChannel.onmessage = handleMessage;
+  //   sendChannel.onclose = handleSendChannelStateChange;
+  // } else {
+  //   // Joiner
+  //   pc.ondatachannel = gotReceiveChannel;
+  // }
 }
 
-function gotDescription1Local(desc) {
-    pc1Local.setLocalDescription(desc);
-    console.log(`Offer from pc1Local\n${desc.sdp}`);
-    pc1Remote.setRemoteDescription(desc);
-    // Since the 'remote' side has no media stream we need
-    // to pass in the right constraints in order for it to
-    // accept the incoming offer of audio and video.
-    pc1Remote.createAnswer().then(gotDescription1Remote, onCreateSessionDescriptionError);
+// // Data channel management
+// function sendData() {
+// var data = sendTextarea.value;
+// if (isInitiator) sendChannel.send(data);
+// else receiveChannel.send(data);
+// trace("Sent data: " + data);
+// }
+
+// Handlers...
+
+function gotReceiveChannel(event) {
+  trace("Receive Channel Callback");
+  receiveChannel = event.channel;
+  receiveChannel.onmessage = handleMessage;
+  receiveChannel.onopen = handleReceiveChannelStateChange;
+  receiveChannel.onclose = handleReceiveChannelStateChange;
 }
 
-function gotDescription1Remote(desc) {
-    pc1Remote.setLocalDescription(desc);
-    console.log(`Answer from pc1Remote\n${desc.sdp}`);
-    pc1Local.setRemoteDescription(desc);
+function handleMessage(event) {
+  trace("Received message: " + event.data);
+  receiveTextarea.value += event.data + "\n";
 }
 
-function gotDescription2Local(desc) {
-    pc2Local.setLocalDescription(desc);
-    console.log(`Offer from pc2Local\n${desc.sdp}`);
-    pc2Remote.setRemoteDescription(desc);
-    // Since the 'remote' side has no media stream we need
-    // to pass in the right constraints in order for it to
-    // accept the incoming offer of audio and video.
-    pc2Remote.createAnswer().then(gotDescription2Remote, onCreateSessionDescriptionError);
+function handleSendChannelStateChange() {
+  var readyState = sendChannel.readyState;
+  trace("Send channel state is: " + readyState);
+  // If channel ready, enable user's input
+  if (readyState == "open") {
+    // dataChannelSend.disabled = false;
+    // dataChannelSend.focus();
+    // dataChannelSend.placeholder = "";
+    // sendButton.disabled = false;
+  } else {
+    // dataChannelSend.disabled = true;
+    // sendButton.disabled = true;
+  }
 }
 
-function gotDescription2Remote(desc) {
-    pc2Remote.setLocalDescription(desc);
-    console.log(`Answer from pc2Remote\n${desc.sdp}`);
-    pc2Local.setRemoteDescription(desc);
+function handleReceiveChannelStateChange() {
+  var readyState = receiveChannel.readyState;
+  trace("Receive channel state is: " + readyState);
+  // If channel ready, enable user's input
+  if (readyState == "open") {
+    // dataChannelSend.disabled = false;
+    // dataChannelSend.focus();
+    // dataChannelSend.placeholder = "";
+    // sendButton.disabled = false;
+  } else {
+    // dataChannelSend.disabled = true;
+    // sendButton.disabled = true;
+  }
 }
+
+// ICE candidates management
+function handleIceCandidate(event) {
+  console.log("handleIceCandidate event: ", event);
+  if (event.candidate) {
+    sendMessage({
+      type: "candidate",
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate,
+    });
+  } else {
+    console.log("End of candidates.");
+  }
+}
+
+// Create Offer
+function doCall() {
+  console.log("Creating Offer...");
+  pc.createOffer(setLocalAndSendMessage, onSignalingError);
+}
+
+// Signalling error handler
+function onSignalingError(error) {
+  console.log("Failed to create signaling message : " + error.name);
+}
+
+// Create Answer
+function doAnswer() {
+  console.log("Sending answer to peer.");
+  pc.createAnswer(setLocalAndSendMessage, onSignalingError);
+}
+
+// Success handler for both createOffer()
+// and createAnswer()
+function setLocalAndSendMessage(sessionDescription) {
+  pc.setLocalDescription(sessionDescription);
+  sendMessage(sessionDescription);
+}
+
+/////////////////////////////////////////////////////////
+// Remote stream handlers...
+
+function handleRemoteStreamAdded(event) {
+  console.log("Remote stream added.");
+  // attachMediaStream(remoteVideo, event.stream);
+  remoteVideo.srcObject = event.stream;
+  remoteStream = event.stream;
+}
+
+// function handleRemoteStreamAdded(e) {
+//   if (video2.srcObject !== e.streams[0]) {
+//     video2.srcObject = e.streams[0];
+//     console.log('pc: received remote stream');
+//   }
+// }
+
+function handleRemoteStreamRemoved(event) {
+  console.log("Remote stream removed. Event: ", event);
+}
+
+function handleRemoteStreamAdded2(event) {
+  console.log("Remote stream added.");
+  // attachMediaStream(remoteVideo2, event.stream);
+  remoteVideo2.srcObject = event.stream;
+  remoteStream2 = event.stream;
+}
+
+// function handleRemoteStreamAdded2(e) {
+//   if (video3.srcObject !== e.streams[0]) {
+//     video3.srcObject = e.streams[0];
+//     console.log('pc2: received remote stream');
+//   }
+// }
+
+function handleRemoteStreamRemoved2(event) {
+  console.log("Remote stream removed. Event: ", event);
+}
+/////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
+// Clean-up functions...
 
 function hangup() {
-    console.log('Ending calls');
-    pc1Local.close();
-    pc1Remote.close();
-    pc2Local.close();
-    pc2Remote.close();
-    pc1Local = pc1Remote = null;
-    pc2Local = pc2Remote = null;
-    hangupButton.disabled = true;
-    callButton.disabled = false;
+  console.log("Hanging up.");
+  stop();
+  sendMessage("bye");
 }
 
-function gotRemoteStream1(e) {
-    if (video2.srcObject !== e.streams[0]) {
-        video2.srcObject = e.streams[0];
-        console.log('pc1: received remote stream');
-    }
+function handleRemoteHangup() {
+  console.log("Session terminated.");
+  stop();
+  isInitiator = false;
 }
 
-function gotRemoteStream2(e) {
-    if (video3.srcObject !== e.streams[0]) {
-        video3.srcObject = e.streams[0];
-        console.log('pc2: received remote stream');
-    }
+function stop() {
+  isStarted = false;
+  if (sendChannel) sendChannel.close();
+  if (receiveChannel) receiveChannel.close();
+  if (pc) pc.close();
+  pc = null;
+  // sendButton.disabled = true;
 }
 
-function iceCallback1Local(event) {
-    handleCandidate(event.candidate, pc1Remote, 'pc1: ', 'local');
-}
+///////////////////////////////////////////
 
-function iceCallback1Remote(event) {
-    handleCandidate(event.candidate, pc1Local, 'pc1: ', 'remote');
-}
-
-function iceCallback2Local(event) {
-    handleCandidate(event.candidate, pc2Remote, 'pc2: ', 'local');
-}
-
-function iceCallback2Remote(event) {
-    handleCandidate(event.candidate, pc2Local, 'pc2: ', 'remote');
-}
-
-function handleCandidate(candidate, dest, prefix, type) {
-    dest.addIceCandidate(candidate)
-        .then(onAddIceCandidateSuccess, onAddIceCandidateError);
-    console.log(`${prefix}New ${type} ICE candidate: ${candidate ? candidate.candidate : '(null)'}`);
-}
-
-function onAddIceCandidateSuccess() {
-    console.log('AddIceCandidate success.');
-}
-
-function onAddIceCandidateError(error) {
-    console.log(`Failed to add ICE candidate: ${error.toString()}`);
-}
---
+++ server.js
@@ -0,0 +1,67 @@
+var static = require("node-static");
+var http = require("http");
+var port = 8080;
+// Create a node-static server instance
+var file = new static.Server();
+
+// We use the http module�s createServer function and
+// rely on our instance of node-static to serve the files
+var app = http
+  .createServer(function (req, res) {
+    file.serve(req, res);
+  })
+  .listen(port);
+
+console.log("Listening on " + app.address().port);
+
+// Use socket.io JavaScript library for real-time web applications
+var io = require("socket.io").listen(app);
+
+// Let's start managing connections...
+io.sockets.on("connection", function (socket) {
+  // Handle 'message' messages
+  socket.on("message", function (message) {
+    log("S --> got message: ", message);
+    // channel-only broadcast...
+    socket.broadcast.emit("message", message);
+  });
+
+  // Handle 'create or join' messages
+  socket.on("create or join", function (room) {
+    // var numClients = io.sockets.clients(room).length;
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom
+      ? Object.keys(clientsInRoom.sockets).length
+      : 0;
+
+    log("S --> Room " + room + " has " + numClients + " client(s)");
+    log("S --> Request to create or join room", room);
+
+    // First client joining...
+    if (numClients == 0) {
+      socket.join(room);
+      socket.emit("created", room);
+    } else if (numClients == 1) {
+      // Second client joining...
+      io.sockets.in(room).emit("join", room);
+      socket.join(room);
+      socket.emit("joined", room);
+    } else if (numClients == 2) {
+      // Second client joining...
+      io.sockets.in(room).emit("join", room);
+      socket.join(room);
+      socket.emit("joined2", room);
+    } else {
+      // max two clients
+      socket.emit("full", room);
+    }
+  });
+
+  function log() {
+    var array = [">>> "];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+    socket.emit("log", array);
+  }
+});
